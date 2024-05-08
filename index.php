614fd41e7e174450c5cd8beed4d071fc2f882052
waqarahmed0980 <wqahmed705@gmail.com>
2024-05-08 16:03:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $books = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Book Gallery</title>
+    <title>MOC Book Printing</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <style>
